abab69ea99d56b892f60e3294eb4044b45fc58a0
BudaNyoni <nyonisbuda@gmail.com>
2019-01-15 15:31:57 +0200

    uyes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { markDirty } from '@angular/core/src/render3';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app';
 }
+
+
+
+// function next(){
+// var mar=$("#slide1").css("margin-left");
+// var nm=mar.replace("px","");
+// if(nm == 0)
+// {
+//   $("right").animeate({"margin-left":"-500px"},"slow");
+// }
+// else if(nm > 0 ||nm!= -200 ){
+// nm = nm-500;
+// $("right")
+// }
+// }
